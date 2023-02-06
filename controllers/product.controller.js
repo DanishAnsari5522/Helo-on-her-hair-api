ae70d4757b71940550ed53239041c57b95a4900e
DanishAnsari5522 <ddanish1928@gmail.com>
2023-02-06 22:14:14 +0530

    Change in product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -10,7 +10,8 @@ const uploadProduct = async (req, res) => {
             return res.status(400).json({ success: false, message: "productdetail , productprice and productcategori are required" })
         } else {
             const data = await upload.create({ productDetail, productPrice, productCategori, productImage })
-            console.log(req.body)
+            // console.log(req.body)
+            res.status(200).json({ data })
         }
 
     } catch (error) {
